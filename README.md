b9301db093f014fc3a15021f5fee2b8d1350073f
Dale Myszewski <dale.myszewski@seagate.com>
2016-08-03 16:53:31 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Electron-Scaffold
+My simple and rather straight forward electron scaffold for the starting projects.
+
+To Use:
+
+1. Install NPM/Node (kinda the big one)
+
+2. npm install (to install deps/modules)
+
+3. npm start (to start it up)
---
+++ main.js
@@ -0,0 +1,13 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+app.on('ready', () => {
+  let win = new BrowserWindow({width:800, height: 600})
+  win.loadURL(`file://${__dirname}/index.html`)
+  //win.webContents.openDevTools() //dev tools - uncomment for the Chrome Dev Toools
+
+  win.on('close', function(){
+    //things that happen when you close the main window
+    win = null;
+  });
+})
